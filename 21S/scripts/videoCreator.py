7efd099fe45d4f986bc4eac47f43108f2fcb2edd
Varin Sikand <varin@ev3everywhere.com>
2021-04-24 15:10:59 -0500

    animated graph

--- 21S/scripts/videoCreator.py
+++ 21S/scripts/videoCreator.py
@@ -5,8 +5,10 @@ import os
 
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 import seaborn as sb
 from pathlib import Path
+import pandas as pd
 
 import cv2
 
@@ -94,14 +96,44 @@ class VRConverter:
     def generateHeatMapCSVs(self):
         self.heat.generateHeatMapCSVs()
 
-def main():
 
+
+def animate(i):
+    frame = data.iloc[:int(i+1)]
+    frame = pd.melt(frame, ignore_index=False, var_name='Type', value_name='Ratio')
+    p = sb.lineplot(x=frame.index, y='Ratio', data=frame, hue='Type')
+    plt.legend(['User Experience', 'Storage'])
+    pass
+
+def main():
+    fig = plt.figure(figsize=(12, 6))
+    sb.set_theme()
+    plt.ylim(0, 1)
     converter = VRConverter(videoId=23, rows=50, cols=100, heatThreshold=.25)
     TEST_FRAMES = [121, 271, 691, 811, 1111, 1351, 1681]
     # converter.makeVideo('compress', 'CompressedSemi23New.avi', 'semiCrcl')
-    userExpPerFrame, userExpPerUser, storagePerFrame = converter.getStats('semiCrcl')
-    for frame in storagePerFrame:
-        print(f'{frame}: {storagePerFrame[frame] * 100}%')
+    userExpPerFrame, userExpPerUser, storagePerFrame  = converter.getStats('linear')
+    # for frame in userExpPerFrame:
+    #     print(f'{frame}: {userExpPerFrame[frame] * 100}%')
+    frames = list(userExpPerFrame.keys())
+    plt.xlim(frames[0], frames[-1])
+    userValues = list(userExpPerFrame.values())
+    storageValues = list(storagePerFrame.values())
+    dataArray = np.array([userValues, storageValues])
+    dataArray = np.transpose(dataArray)
+    # print(dataArray)
+    global data
+    data = pd.DataFrame(dataArray, index=frames, columns=['User Experience', 'Storage'])
+    
+    # print(pd.melt(data, ignore_index=False, var_name='Type', value_name='Ratio'))
+    # ax = sb.lineplot(data=data)
+    ani = animation.FuncAnimation(fig, animate, frames=len(frames), repeat=True)
+    
+    # ani.save()
+    
+    plt.show()
+    # plt.plot(frames, values)
+    # plt.show()
     # converter.generateTestMaps(TEST_FRAMES)
     # converter.generateHeatMapCSVs()
 
